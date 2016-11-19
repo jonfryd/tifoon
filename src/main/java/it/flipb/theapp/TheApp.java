c1f51916cac814eff5a91a10cc0700d4ce6037c2
Jon Frydensbjerg <jonf@vip.cybercity.dk>
2016-11-19 11:23:20 +0100

    Dummy Spring Boot app added

---
+++ src/main/java/it/flipb/theapp/TheApp.java
@@ -0,0 +1,11 @@
+package it.flipb.theapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TheApp.class, args);
+    }
+}
